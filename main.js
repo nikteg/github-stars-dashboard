0e38f08d3403dbf1fec1377814f4301f0bf96037
Niklas Tegnander <niklas.tegnander@stenaline.com>
2020-05-27 16:12:15 +0200

    Check that element exists before modifying them + Prettier

--- main.js
+++ main.js
@@ -1,61 +1,69 @@
-(function(){
-  var $starsUrl = document.querySelector(".header-nav-current-user a")
+(function () {
+  var $starsUrl = document.querySelector(".header-nav-current-user a");
 
   if (!$starsUrl) {
-    console.debug("GitHub Stars Dashboard extension skipped")
-    console.debug('Could not find user dropdown $(".header-nav-current-user a")')
+    console.debug("GitHub Stars Dashboard extension skipped");
+    console.debug(
+      'Could not find user dropdown $(".header-nav-current-user a")'
+    );
 
-    return
+    return;
   }
 
-  var starsUrl = $starsUrl.href + "?tab=stars"
-  var $dashboardSidebar = document.querySelector(".dashboard-sidebar.js-sticky")
+  var starsUrl = $starsUrl.href + "?tab=stars";
+  var $dashboardSidebar = document.querySelector(
+    ".dashboard-sidebar.js-sticky"
+  );
 
-  console.debug("GitHub Stars Dashboard extension loaded")
+  if (!$dashboardSidebar) {
+    console.debug("Could not find sidebar. Exiting.");
+    return;
+  }
+
+  console.debug("GitHub Stars Dashboard extension loaded");
 
   // <h2 class="hide-sm hide-md f5 mb-1 border-top pt-3">
   //  <a href="https://github.com/username?tab=stars">Your stars</a>
   // </h2>
-  $starsHeading = document.createElement("h2")
-  $starsHeading.classList = "hide-sm hide-md f5 mb-1 border-top pt-3"
-  $starsHeadingLink = document.createElement("a")
-  $starsHeadingLink.classList = "link-gray-dark"
-  $starsHeadingLink.innerText = "Your stars"
-  $starsHeadingLink.href = starsUrl
-  $starsHeading.appendChild($starsHeadingLink)
+  $starsHeading = document.createElement("h2");
+  $starsHeading.classList = "hide-sm hide-md f5 mb-1 border-top pt-3";
+  $starsHeadingLink = document.createElement("a");
+  $starsHeadingLink.classList = "link-gray-dark";
+  $starsHeadingLink.innerText = "Your stars";
+  $starsHeadingLink.href = starsUrl;
+  $starsHeading.appendChild($starsHeadingLink);
 
-  $dashboardSidebar.appendChild($starsHeading)
+  $dashboardSidebar.appendChild($starsHeading);
 
-  $loading = document.createElement("p")
-  $loading.classList = "notice"
-  $loading.innerText = "Loading stars..."
+  $loading = document.createElement("p");
+  $loading.classList = "notice";
+  $loading.innerText = "Loading stars...";
 
-  $dashboardSidebar.appendChild($loading)
+  $dashboardSidebar.appendChild($loading);
 
   fetch(starsUrl)
-    .then(res => res.text())
-    .then(text => {
-      var parser = new DOMParser()
+    .then((res) => res.text())
+    .then((text) => new DOMParser().parseFromString(text, "text/html"))
+    .then((doc) => {
+      var $stars = doc.querySelectorAll(
+        ".col-12.d-block.width-full.py-4.border-bottom"
+      );
 
-      return parser.parseFromString(text, "text/html")
-    })
-    .then(doc => {
-      var $stars = doc.querySelectorAll(".col-12.d-block.width-full.py-4.border-bottom")
+      $loading.remove();
+      $starsHeading.classList.remove("mb-1"); // Remove bottom border
 
-      $loading.remove()
-      $starsHeading.classList.remove("mb-1") // Remove bottom border
+      $stars.forEach(($star) => {
+        $star.classList.replace("py-4", "py-3");
+        const $heading = $star.querySelector("h3");
+        if ($heading) $heading.style.fontSize = "14px";
+        const $paragraph = $star.querySelector("p");
+        if ($paragraph) $paragraph.style.fontSize = "13px";
 
-      $stars.forEach($star => {
-        $star.classList.replace("py-4", "py-3")
-        $star.querySelector("h3").style.fontSize = "14px"
+        // Remove star/unstar
+        const $buttonContainer = $star.querySelector(".float-right");
+        if ($buttonContainer) $buttonContainer.remove();
 
-        $description = $star.querySelector("p")
-        if ($description) {
-          $description.style.fontSize = "13px"
-        }
-
-        $star.querySelector(".float-right").remove()
-        $dashboardSidebar.appendChild($star)
-      })
-    })
-})()
+        $dashboardSidebar.appendChild($star);
+      });
+    });
+})();
